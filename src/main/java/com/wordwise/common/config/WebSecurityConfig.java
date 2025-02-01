8ea46e6dc0e4aa95793fda5e15a1781448a00ce0
kang-sumin <min001117@gmail.com>
2025-02-01 23:58:31 +0900

    [FEAT] created Spring Security

---
+++ src/main/java/com/wordwise/common/config/WebSecurityConfig.java
@@ -0,0 +1,55 @@
+package com.wordwise.common.config;
+
+import com.wordwise.common.enums.UserRole;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestFilter;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+public class WebSecurityConfig {
+
+    private final JwtSecurityFilter jwtSecurityFilter;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())  // CSRF 비활성화
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtSecurityFilter, SecurityContextHolderAwareRequestFilter.class)
+                .formLogin(form -> form.disable())
+//                .anonymous(anonymous -> anonymous.disable())  // payment 프론트 테스트 위해 익명 접근 가동하도록 주석처리 하겠습니다.
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .logout(logout -> logout.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/",
+                                "/api/v1/auth/kakao/callback",
+                                "/api/v1/auth/kakao"
+                        )
+                        .permitAll()
+                        .requestMatchers(
+                                // 어드민 권한 api
+                                "/api/admin"
+                        )
+                        .hasAuthority(UserRole.Authority.ADMIN)
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+}
