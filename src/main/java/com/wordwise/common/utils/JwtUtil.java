64a1047437a68787d09e9139fe82e8ccc5ca3bd0
kang-sumin <min001117@gmail.com>
2025-02-01 21:35:16 +0900

    [FEAT] created JwtUtil class

---
+++ src/main/java/com/wordwise/common/utils/JwtUtil.java
@@ -0,0 +1,68 @@
+package com.wordwise.common.utils;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import com.wordwise.common.enums.UserRole;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import java.util.NoSuchElementException;
+
+@Component
+public class JwtUtil {
+
+    private static final String BEARER_PREFIX = "Bearer ";
+    private static final long TOKEN_TIME = 60 * 60 * 1000L; // 토큰 만료 시간 : 60분
+
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+    private Key key;
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+    @PostConstruct
+    public void init() {
+        byte[] bytes = Base64.getDecoder().decode(secretKey);
+        key = Keys.hmacShaKeyFor(bytes);
+    }
+
+    // 토큰 생성
+    public String createToken(Long userId, String email, UserRole userRole) {
+        Date date = new Date();
+
+        return BEARER_PREFIX +
+                Jwts.builder()
+                        .setSubject(String.valueOf(userId)) // userId
+                        .claim("email", email)  // 사용자 email
+                        .claim("userRole", userRole.name()) // 사용자 권한
+                        .setExpiration(new Date(date.getTime() + TOKEN_TIME))   // 토큰 만료 시간
+                        .setIssuedAt(date) // 발급일
+                        .signWith(key, signatureAlgorithm) // 암호화 알고리즘
+                        .compact();
+    }
+
+    // JWT 토큰 substring
+    public String substringToken(String tokenValue) {
+        if (StringUtils.hasText(tokenValue) && tokenValue.startsWith(BEARER_PREFIX)) {
+            return tokenValue.substring(7);
+        }
+        throw new NoSuchElementException("Not Found Token");
+    }
+
+    // 토큰에서 사용자 정보 추출
+    public Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
