1de70ddfc53baa0b9319ea6a0df0eb849163e4c7
kang-sumin <min001117@gmail.com>
2025-02-06 16:02:54 +0900

    [FEAT] CORS Config 구현 및 Security cors 적용

--- src/main/java/com/wordwise/common/config/CorsConfig.java
+++ src/main/java/com/wordwise/common/config/CorsConfig.java
@@ -1,13 +1,11 @@
 package com.wordwise.common.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @Configuration
