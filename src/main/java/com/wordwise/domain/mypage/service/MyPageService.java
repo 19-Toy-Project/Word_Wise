369903e9871b4621befc1b2360af3d13c5730e75
kang-sumin <min001117@gmail.com>
2025-02-10 15:39:20 +0900

    [FEAT] 사용자가 찜한 문장 조회 API

--- src/main/java/com/wordwise/domain/mypage/service/MyPageService.java
+++ src/main/java/com/wordwise/domain/mypage/service/MyPageService.java
@@ -4,10 +4,16 @@ import com.wordwise.common.apipayload.status.ErrorStatus;
 import com.wordwise.common.exception.ApiException;
 import com.wordwise.domain.auth.AuthUser;
 import com.wordwise.domain.mypage.dto.UserAverageScoreDto;
+import com.wordwise.domain.mypage.dto.UserWishSentenceDto;
 import com.wordwise.domain.mypage.response.UserAverageScoreResponse;
+import com.wordwise.domain.mypage.response.UserWishSentenceListResponse;
 import com.wordwise.domain.sentence.entity.Score;
 import com.wordwise.domain.sentence.repository.ScoreRepository;
+import com.wordwise.domain.sentence.repository.WishRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,7 +25,9 @@ import java.util.stream.Collectors;
 public class MyPageService {
 
     private final ScoreRepository scoreRepository;
+    private final WishRepository wishRepository;
 
+    // 사용자 학습 평균 점수 조회
     public UserAverageScoreResponse getUserAverageScore(AuthUser authUser) {
         // 사용자 Score 객체 가져오기
         List<Score> userScore = scoreRepository.findByUserIdOrderByType(authUser.getId());
@@ -37,5 +45,24 @@ public class MyPageService {
         )).collect(Collectors.toList());
 
         return UserAverageScoreResponse.from(content);
+    }
+
+    // 사용자가 찜한 문장 조회
+    public Page<UserWishSentenceListResponse> getUserWishSentenceList(AuthUser authUser, int page, int size) {
+        Pageable pageable = PageRequest.of(page-1, size);
+
+        Page<UserWishSentenceDto> userWishSentences = wishRepository.findAllUserWishSentence(authUser.getId(), pageable);
+
+        // 찜한 문장이 없을때
+        if(userWishSentences.getContent().isEmpty()) {
+            throw new ApiException(ErrorStatus._NOT_EXIST_WISH_SENTENCE);
+        }
+
+        return userWishSentences.map(u -> UserWishSentenceListResponse.of(
+                u.getWordId(),
+                u.getSentenceId(),
+                u.getSentence(),
+                u.getSentence_kr()
+        ));
     }
 }
--- src/main/java/com/wordwise/domain/sentence/repository/WishRepository.java
+++ src/main/java/com/wordwise/domain/sentence/repository/WishRepository.java
@@ -1,10 +1,22 @@
 package com.wordwise.domain.sentence.repository;
 
+import com.wordwise.domain.mypage.dto.UserWishSentenceDto;
 import com.wordwise.domain.sentence.entity.Wish;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface WishRepository extends JpaRepository<Wish,Long> {
     Wish findBySentenceIdAndUserId(Long id, Long id1);
 
     Boolean existsBySentenceIdAndUserId(Long id, Long id1);
+
+    @Query("SELECT new com.wordwise.domain.mypage.dto.UserWishSentenceDto(s.word.id, s.id, s.sentence_en, s.sentence_kr)" +
+            "FROM Wish w JOIN Sentence s " +
+            "ON w.sentence.id = s.id " +
+            "WHERE w.user.id = :userId"
+    )
+    Page<UserWishSentenceDto> findAllUserWishSentence(@Param("userId") Long userId, Pageable pageable);
 }
