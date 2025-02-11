d723e08da725749860446224407da565d345c33d
kang-sumin <min001117@gmail.com>
2025-02-11 18:39:19 +0900

    [FEAT] RefreshToken으로  AccessToken 발급 메서드

--- src/main/java/com/wordwise/domain/auth/service/AuthService.java
+++ src/main/java/com/wordwise/domain/auth/service/AuthService.java
@@ -1,7 +1,12 @@
 package com.wordwise.domain.auth.service;
 
+import com.wordwise.common.apipayload.status.ErrorStatus;
+import com.wordwise.common.enums.UserRole;
+import com.wordwise.common.exception.ApiException;
+import com.wordwise.common.utils.JwtUtil;
 import com.wordwise.domain.auth.entity.RefreshToken;
 import com.wordwise.domain.auth.repository.RefreshTokenRepository;
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,13 +17,36 @@ import org.springframework.transaction.annotation.Transactional;
 public class AuthService {
 
     private final RefreshTokenRepository refreshTokenRepository;
+    private final JwtUtil jwtUtil;
 
+    // Refresh Token 저장
     @Transactional
     public void saveRefreshToken(Long userId, String refreshToken) {
 
         RefreshToken refreshTokenEntity = RefreshToken.of(userId, refreshToken);
 
         refreshTokenRepository.save(refreshTokenEntity);
+    }
+
+    // Refresh Token으로 Access Token 발급
+    public String refreshAccessToken(String refreshToken) {
+
+        // 토큰에서 사용자 정보 추출
+        Claims claims = jwtUtil.extractClaims(refreshToken);
+        Long userId = Long.valueOf(claims.getSubject());
+
+        RefreshToken storedToken = refreshTokenRepository.findByUserId(userId)
+                .orElseThrow(()-> new ApiException(ErrorStatus._INVALID_REFRESH_TOKEN));
+
+        // 저장된 Refresh Token이랑 동일한지 확인
+        if(!storedToken.getRefreshToken().equals(refreshToken)) {
+            throw new ApiException(ErrorStatus._MISMATCHED_REFRESH_TOKEN);
+        }
+
+        // Refresh Token으로 AccessToken 발급
+        String refreshAccessToken = jwtUtil.createAccessToken(userId, claims.get("email", String.class), UserRole.valueOf(claims.get("userRole", String.class)));
+
+        return refreshAccessToken;
     }
 
 
