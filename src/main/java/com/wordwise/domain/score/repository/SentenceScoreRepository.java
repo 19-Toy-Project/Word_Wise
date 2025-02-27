89bcd2fe5b45bfb4ee9bae18a6beb29f8ad99f1d
kang-sumin <min001117@gmail.com>
2025-02-27 16:01:33 +0900

    [FEAT] 최신 등록된 문장 점수 조회 JPQL

--- src/main/java/com/wordwise/domain/score/repository/SentenceScoreRepository.java
+++ src/main/java/com/wordwise/domain/score/repository/SentenceScoreRepository.java
@@ -2,6 +2,16 @@ package com.wordwise.domain.score.repository;
 
 import com.wordwise.domain.score.entity.SentenceScore;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface SentenceScoreRepository extends JpaRepository<SentenceScore, Long> {
+
+    @Query("SELECT s FROM SentenceScore s " +
+            "WHERE s.user.id = :userId AND s.sentence.id = :sentenceId " +
+            "ORDER BY s.modifiedAt DESC " +
+            "LIMIT 1")
+    Optional<SentenceScore> findLatestSentence(@Param("userId") Long userId, @Param("sentenceId") Long sentenceId);
 }
