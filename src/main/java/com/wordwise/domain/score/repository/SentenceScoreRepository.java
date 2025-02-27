a658a7f2e85396bf74e7ce17bc7955288fe26218
kang-sumin <min001117@gmail.com>
2025-02-27 17:07:57 +0900

    [FEAT] 반환타입 변환

--- src/main/java/com/wordwise/domain/score/repository/SentenceScoreRepository.java
+++ src/main/java/com/wordwise/domain/score/repository/SentenceScoreRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface SentenceScoreRepository extends JpaRepository<SentenceScore, Long> {
 
@@ -13,5 +15,17 @@ public interface SentenceScoreRepository extends JpaRepository<SentenceScore, Lo
             "WHERE s.user.id = :userId AND s.sentence.id = :sentenceId " +
             "ORDER BY s.modifiedAt DESC " +
             "LIMIT 1")
-    SentenceScore findLatestSentence(@Param("userId") Long userId, @Param("sentenceId") Long sentenceId);
+    SentenceScore findLatestSentence(
+            @Param("userId") Long userId,
+            @Param("sentenceId") Long sentenceId
+    );
+
+    @Query("SELECT DISTINCT DATE(s.createdAt) FROM SentenceScore s " +
+            "WHERE s.user.id = :userId AND s.createdAt BETWEEN :startDate AND :endDate " +
+            "ORDER BY DATE(s.createdAt) ASC")
+    List<LocalDate> findStudyDate(
+            @Param("userId") Long id,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate
+    );
 }
